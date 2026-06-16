3e5a09660f92318e4d1bc71a7c75e8028e509142
pawan <dhuriya.pawan@gmail.com>
2026-06-16 13:16:24 +0530

    test commit

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -11,7 +11,7 @@ resource "aws_db_instance" "postgres" {
   username                    = var.username
   password                    = var.password
   port                        = 5432
-  db_subnet_group_name        = aws_db_subnet_group.db_subnets.name
+  db_subnet_group_name        = aws_db_subnet_group.db_subnets[count.index]
   parameter_group_name        = aws_db_parameter_group.pg.name
   vpc_security_group_ids      = [var.security_group_id]
   storage_encrypted           = true
