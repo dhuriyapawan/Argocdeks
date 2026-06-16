4be126c4bdce28d3764f37d3786ba17cefe5786f
pawan <dhuriya.pawan@gmail.com>
2026-06-16 11:27:38 +0530

    test commit

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -32,7 +32,9 @@ data "tls_certificate" "eks" {
 }
 
 resource "aws_iam_openid_connect_provider" "eks" {
-  client_id_list_value = ["sts.amazonaws.com"]
-  thumbprint_list      = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url                  = aws_eks_cluster.main.identity[0].oidc[0].issuer
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
